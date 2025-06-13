6d69e671488c1fa85795e72ec532fa03372d379e
Maik Wypior <160053500+MaikWpr83@users.noreply.github.com>
2025-06-13 22:11:39 +0200

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://join-229-c9c59-default-rtdb.europe-west1.firebasedatabase.app';
+const BASE_URL = 'https://join-8ee38-default-rtdb.europe-west1.firebasedatabase.app';
 let profileColor = ['#FF7A00', '#FF5EB3', '#6E52FF', '#9327FF', '#00BEE8', '#1FD7C1', '#FF745E', '#FFA35E', '#FC71FF', '#FFC701', '#0038FF', '#C3FF2B', '#FFE62B', '#FF4646', '#FFBB2B']
 let contacts = [];
 let tasks = [];
